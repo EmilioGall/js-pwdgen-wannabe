2056f6f50e0ac976442d5df0782f16ad85dbb55d
Emilio Gallo <emilio.gallo17@gmail.com>
2024-03-22 21:11:09 +0100

    add: psw generator - js

--- js/script.js
+++ js/script.js
@@ -1,4 +1,29 @@
-// DEBUG
+// DATA COLLECTION
 
-alert("Ciao!");
+// Name Input
 
+const userName = prompt("Ciao, Utente! Qual è il tuo nome?"); // String
+
+console.log(userName, typeof userName); // ShowInConsoleName Input
+
+// Surname Input
+
+const userSurname = prompt(`Perfetto, ${userName}! E il tuo cognome?`); // String
+
+console.log(userSurname, typeof userSurname); // ShowInConsole SurnameInput
+
+// Color Input
+
+const userColor = prompt(`Ultimo passaggio, ${userName}! Qual è il tuo colore preferito?`); // String
+
+console.log(userColor, typeof userColor); // ShowInConsole ColorInput
+
+// PASSWORD GENERATION
+
+let pswGenerated = `
+  ${userName}${userSurname}${userColor}${"21"}
+`; // String
+
+// PASSWORD OUTPUT
+
+document.getElementById("psw").innerHTML = pswGenerated;
